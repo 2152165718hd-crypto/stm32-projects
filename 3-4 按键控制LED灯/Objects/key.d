e0950b18e44bf6d3869521801211a4e35c407ea4
Hedong66 <2152165718hd@gmail.com>
2025-12-27 23:32:20 +0800

    updata

--- 3-4 按键控制LED灯/Objects/key.d
+++ 3-4 按键控制LED灯/Objects/key.d
@@ -1,7 +1,7 @@
 .\objects\key.o: Hardware\Key.c
 .\objects\key.o: .\Start\stm32f10x.h
 .\objects\key.o: .\Start\core_cm3.h
-.\objects\key.o: E:\Keil5\Keil5-Stm32\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\key.o: E:\Keil5\Keil5_MDK\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\key.o: .\Start\system_stm32f10x.h
 .\objects\key.o: .\User\stm32f10x_conf.h
 .\objects\key.o: .\Library\stm32f10x_adc.h
